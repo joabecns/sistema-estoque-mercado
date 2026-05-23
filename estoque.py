f2ecb710a247719f75e61c7bf624415d4d778cd5
Joabe Carvalho <96736137+joabecns@users.noreply.github.com>
2026-05-23 15:48:25 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 arquivo = "estoque.json"
 
@@ -17,33 +18,89 @@ estoque_padrao = {
 
 
 def mostrar_estoque(estoque_padrao: dict):
-
     print("=" * 50)
     print(f"{'PRODUTO':<20}{'PREÇO':<10}{'QUANTIDADE':<10}")
     print("=" * 50)
 
     for produto, dados in sorted(estoque_padrao.items()):
-
         print(f"{produto:<20}{dados['preco']:<10}{dados['quantidade']:<10}")
 
     print("=" * 50)
 
 
 def salvar_estoque(estoque_padrao: dict):
-
-    with open(arquivo, "w", encoding="utf-8") as arq:
-
-        json.dump(estoque_padrao, arq, indent=4, ensure_ascii=False)
+    with open("estoque.json", "w", encoding="utf-8") as arquivo:
+        json.dump(estoque_padrao, arquivo, indent=4, ensure_ascii=False)
 
 
 def carregar_estoque():
+    if not os.path.exists("estoque.json"):
+        salvar_estoque(estoque_padrao)
+        return estoque_padrao
 
-    with open(arquivo, "r", encoding="utf-8") as arq:
+    with open("estoque.json", "r", encoding="utf-8") as arquivo:
+        estoque = json.load(arquivo)
+        return estoque
 
-        estoque = json.load(arq)
+def adicionar_item(estoque: dict):
+    print()
+    print("--- ADICIONAR / ATUALIZAR PRODUTO ---")
+    nome = input("Nome do produto: ").strip().lower()
 
-    return estoque
+    if not nome:
+        print("Nome inválido.")
+        return
+    
+    while True:
+        try:
+            preco = float(input("Preço (R$): ").replace(",", "."))
+            if preco <= 0:
+                print('Valor inváido! Insira novamente')
+            else:
+                break
+        except ValueError:
+            print("Preço inválido.")
+            return
+        
+    while True:
+        try:
+            quantidade = int(input("Quantidade em estoque: "))
+            if quantidade <= 0:
+                print('Valor inváido! Insira novamente')
+            else:
+                break
+        except ValueError:
+            print("Quantidade inválida.")
+            return
 
-salvar_estoque(estoque_padrao)
+    if nome in estoque:
+        status = 'atualizado'
+    else:
+        status = 'adicionado'
+    estoque[nome] = {"preco": preco, "quantidade": quantidade}
+    salvar_estoque(estoque)
+    print(f"{nome} foi {status} com sucesso!")
+
+
+def remover_item(estoque: dict):
+    print()
+    print("--- REMOVER PRODUTO ---")
+    mostrar_estoque(estoque)
+    print()
+    nome = input("Nome do produto a remover: ").strip().lower()
+
+    if nome in estoque:
+        confirma = input(f"Confirma remoção de '{nome}'? (s/n): ").strip().lower()[0]
+        if confirma == "s":
+            del estoque[nome]
+            salvar_estoque(estoque)
+            print(f"O produto '{nome}' foi removido")
+        else:
+            print("Operação cancelada.")
+    else:
+        print("Produto não encontrado no estoque")
+
 estoque = carregar_estoque()
 mostrar_estoque(estoque)
+adicionar_item(estoque)
+remover_item(estoque)
