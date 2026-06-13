666bcaf5d0731ad79da6615e7746dd0fda9824fc
Joabe Carvalho <96736137+joabecns@users.noreply.github.com>
2026-06-13 13:08:52 -0300

    Refactor main menu and error handling
    Updated the menu display and error messages for better user experience.

--- main.py
+++ main.py
@@ -2,30 +2,28 @@ import estoque
 from time import sleep
 
 def main(dados_estoque: dict):
-    print("=" * 40)
-    print("SUPERMERCADO DO SEU ZÉ".center(35))
-    print("=" * 40)
-    print()
-
     while True:
-        print("=" * 40)
-        print("MENU DO ADMINISTRADOR".center(35))
+        print("\n" + "=" * 40)
+        print("SUPERMERCADO DO SEU ZÉ".center(40))
+        print(f"{'\033[1;36m'}MENU DO ADMINISTRADOR{'\033[m'}".center(49))
         print("=" * 40)
         print("[1] Ver estoque completo")
         print("[2] Adicionar / atualizar produto")
         print("[3] Remover produto")
         print("[0] Sair")
-        print()
+        print("=" * 40)
 
         try:
             opcao = int(input("Insira o número da opção desejada: "))
         except ValueError:
-            print("Digite um número, não letras")
+            print(f"{'\033[1;31m'}Erro: Digite apenas números, não letras.{'\033[m'}")
             print()
             continue
 
         if opcao == 1:
             estoque.mostrar_estoque(dados_estoque)
+            input(f'Aperte {'\033[1m'}ENTER{'\033[m'} para voltar ao MENU...')
+            print()
 
         elif opcao == 2:
             estoque.adicionar_item(dados_estoque)
@@ -36,9 +34,10 @@ def main(dados_estoque: dict):
         elif opcao == 0:
             print("Saindo do painel admim...") 
             sleep(1.5)
+            print(f"{'\033[1m'}Até mais!{'\033[m'}")
             break
         else:
-            print("Opção inválida.")
+            print("Opção inválida. Tente novamente!")
 
 
 dados_estoque = estoque.carregar_estoque()
